60d77861a26ae486102244f7ddaf41b471428b41
Rafael Souza <rasiso@gmail.com>
2019-08-03 14:56:34 -0300

    Criação classe Calculadora

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Calculadora {
+
+    public int add(String numbers) {
+        int soma = 0;
+        List<String> itens = Arrays.asList(numbers.split(","));
+
+            for (int i = 0; i < itens.size(); i++) {
+                try {
+                    soma += Integer.parseInt(itens.get(i));
+                } catch (NumberFormatException e){
+                   continue;
+                }
+            }
+        return soma;
+    }
+}
